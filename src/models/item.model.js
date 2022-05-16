2c7cc9babc539a76c8ca59ade43ee45fb46c80c2
tharushikadealwis <it19016108@my.sliit.lk>
2022-05-16 20:51:31 +0530

    k8s config

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
     id:{ type: String, required: true, trim: true},
-    name:{ type: String, required: true, trim: true},
+    producer:{ type: String, required: true, trim: true},
+    itemName:{ type: String, required: true, trim: true},
     quantity:{ type: Number, required: true, trim: true},
     price:{ type: Number, required: true, trim: true},
 
