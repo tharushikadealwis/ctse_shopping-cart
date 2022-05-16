a1a3f472337131124c5c82de6d967112b9bb4fb4
tharushikadealwis <it19016108@my.sliit.lk>
2022-05-16 16:44:46 +0530

    action docker

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
     id:{ type: String, required: true, trim: true},
+    name:{ type: String, required: true, trim: true},
+    quantity:{ type: Number, required: true, trim: true},
+    price:{ type: Number, required: true, trim: true},
+
 })
 
 const Item = mongoose.model('items', ItemSchema);
